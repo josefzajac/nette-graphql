16f5c988eafe65bf975b5c1e6b9b9a0e327dcab1
Josef Zajac <josef.zajac@gmail.com>
2023-11-05 15:53:52 +0100

    Local run fixes

--- db/Migrations/Version20231102141438.php
+++ db/Migrations/Version20231102141438.php
@@ -17,16 +17,15 @@ final class Version20231102141438 extends AbstractMigration
         return '';
     }
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE brand (id INT NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-    }
+	public function up(Schema $schema): void
+	{
+		$this->addSql('CREATE SEQUENCE brand_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+		$this->addSql('CREATE TABLE brand (id INT NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+	}
 
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE brand');
-    }
+	public function down(Schema $schema): void
+	{
+		$this->addSql('DROP TABLE brand');
+		$this->addSql('DROP SEQUENCE brand_id_seq CASCADE');
+	}
 }
